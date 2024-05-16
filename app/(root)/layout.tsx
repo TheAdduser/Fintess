f8ebdbf435e9e7dff79c1b40a921690cca11cbc4
TheAdduser <EasyRider190@gmail.com>
2024-05-16 15:25:26 +0200

    update sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,15 @@
+import Sidebar from "@/components/Sidebar";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = { firstName: 'Bartek', lastName: 'Wrona'};
+  
   return (
-    <main>
-        SIDEABAR
+    <main className="flex h-screen w-full font-inter">
+        <Sidebar user={loggedIn}/>
         {children}
     </main>
   );
