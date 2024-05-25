837f37528b47cd927ae4682fcedb29ebcc43c20d
TheAdduser <EasyRider190@gmail.com>
2024-05-25 12:30:30 +0200

    Changeg hidden card number symbols

--- components/BankCard.tsx
+++ components/BankCard.tsx
@@ -26,7 +26,7 @@ const BankCard = ({account, userName, showBalance = true}: CreditCardProps) => {
               </h2>
             </div>
             <p className="text-14 font-semibold tracking-[1.1px] text-white">
-              **** **** ****
+            ●●●● ●●●● ●●●●
               <span className="text-16">
                 {account.mask}
               </span>
